4f1e6ee5d654c1e3a10600d4b8aa4aa21910dfc0
nick brown <nick@browndesign.co.uk>
2020-12-09 10:48:12 +0000

    hyperlink added

--- 1 string1 hello name.py
+++ 1 string1 hello name.py
@@ -2,11 +2,18 @@
 # https://codingbat.com/prob/p115413
 
 from tkinter import *
+import webbrowser
  
 root = Tk()
  
 e = Entry(root, width=25, borderwidth=15,font=("Courier", 44))
 e.grid(row=0,column=0,columnspan=3,padx=10,pady=10)
+
+#create link to problem
+link1 = Label(root, text="codingbat exercise", fg="blue", cursor="hand2")
+link1.bind("<Button-1>", lambda e: webbrowser.open_new(r"https://codingbat.com/prob/p115413"))
+link1.grid(row=6,column=0,columnspan=3)
+
  
 def button_click():
    
